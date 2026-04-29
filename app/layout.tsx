83da460830a3bd3d6db43f49cacbb1a6715d64d7
shoaibvhmed <sho.md50@gmail.com>
2026-04-29 13:20:29 +0530

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,34 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Flywheel",
-  description: "Direct mail reinvented for modern ecommerce.",
+  title: "AI-first Marketing That Drives Revenue - Flywheel Media",
+  description:
+    "Flywheel is an AI-first marketing agency helping orthodontic practices, real estate firms, and interior design brands generate leads and predictable growth.",
+
+  openGraph: {
+    title: "Flywheel Media",
+    description:
+      "AI-powered marketing that drives leads, pipelines, and revenue.",
+    url: "https://www.flyweel.media",
+    siteName: "Flywheel Media",
+    images: [
+      {
+        url: "/assets/company/banner.png", // 👈 your image
+        width: 1200,
+        height: 630,
+        alt: "Flywheel Marketing Banner image",
+      },
+    ],
+    type: "website",
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    title: "Flywheel Media",
+    description:
+      "AI-first marketing agency for predictable growth.",
+    images: ["/assets/company/banner.png"],
+  },
 };
 
 export default function RootLayout({
